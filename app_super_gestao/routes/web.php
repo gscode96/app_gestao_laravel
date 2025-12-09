368c075052d3ee4ec31aa1d586741321f9882860
Gabriel Santos <gs.code96@gmail.com>
2025-12-09 08:50:00 -0300

    feat: criado envio de parametros nas rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -7,3 +7,14 @@ Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos' ]);
 
 Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato' ]);
+
+Route::get(
+    '/contato/{nome}/{categoria}/{assunto}/{mensagem}',
+    function (string $nome, string $categoria, string $assunto, string $mensagem) {
+       echo "Estamos aqui para ajudar, $nome.<br>
+       Sua mensagem foi enviada com sucesso!<br>
+       Categoria: $categoria<br>
+       Assunto : $assunto<br>
+       Mensagem: $mensagem";    
+    }
+);
