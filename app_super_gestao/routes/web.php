761722a4c91b2d9d1d1ffa16b58269fffb9e9ca9
Gabriel Santos <gs.code96@gmail.com>
2025-12-09 09:18:11 -0300

    feat: removido rota de teste e adicionado lista nas views

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -7,15 +7,3 @@ Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos' ]);
 
 Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato' ]);
-
-Route::get(
-    '/contato/{nome?}/{categoria_id?}',
-    function (
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1,
-    ) {
-        echo "Estamos aqui para ajudar, $nome.<br>
-       Categoria: $categoria_id<br>";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+'); //
-// Expressão regular para aceitar apenas números na categoria e letras no nome
