40087f4cfd30f1cefa3aea9be1fb9a7890332da5
Gabriel Santos <gs.code96@gmail.com>
2025-12-09 09:35:23 -0300

    feat: nomeando rotas e utilizando route nas views

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,29 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
-
-Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos' ]);
-
-Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato' ]);
-
-Route::get('/login', function () {
-    return 'Login';
-});
-
-
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobreNos');
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato' ])->name('site.contato');
+Route::get('/login', function () {return 'Login';})->name('site.login');
 
 // Grupo de rotas com prefixo /app para separar de outras rotas
 Route::prefix('/app')->group(function () {
-    Route::get('/clientes', function () {
-        return 'Clientes';
-    });
-
-    Route::get('/fornecedores', function () {
-        return 'Fornecedores';
-    });
-
-    Route::get('/produtos', function () {
-        return 'Produtos';
-    });
+    Route::get('/clientes', function () {return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function () {return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function () {return 'Produtos';})->name('app.produtos');
 });
