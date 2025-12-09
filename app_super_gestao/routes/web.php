0f382165e269a2f91f3dd3db4b0e77962b2cd15b
Gabriel Santos <gs.code96@gmail.com>
2025-12-09 09:45:14 -0300

    flow: implementando rota de fallback para caso rota acessa nao exista

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -23,3 +23,9 @@ Route::get('/rota1', function () {
 Route::get('/rota2', function () {
     return redirect()->route('site.rota1');
 })->name('site.rota2');
+
+
+// rota fallback (rota que é chamada quando nenhuma outra rota é encontrada)
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial.';
+});
