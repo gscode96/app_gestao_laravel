387d1e661c6b2acdb9d50a000f0fb610581eca6b
Gabriel Santos <gs.code96@gmail.com>
2025-12-10 09:30:39 -0300

    feat: testando sintaxe blade e php puro na view

---
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,14 @@
+<h3>Fornecedores</h3>
+
+{{-- Comentario da sintaxe blade --}}
+
+{{ "teste fornecedor blade" }}
+
+
+@php
+
+#tudo aqui dentro é igual o php
+
+echo 'teste fornecedor php';
+
+@endphp
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/login', function () {return 'Login';})->name('site.login');
 // Grupo de rotas com prefixo /app para separar de outras rotas
 Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', function () {return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores', [App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedores');
     Route::get('/produtos', function () {return 'Produtos';})->name('app.produtos');
 });
 
