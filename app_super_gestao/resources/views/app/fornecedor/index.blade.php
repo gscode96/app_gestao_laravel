03dbb94eb6a1860734a7191c077994de6d3baf4d
Gabriel Santos <gs.code96@gmail.com>
2025-12-12 21:18:45 -0300

    feat: utilizando isset para verificar conteudo da variavel

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -12,17 +12,23 @@
 {{--  @dd(fornecedores) print o array  --}}     
 {{--  @unless executa quando o retorno for false  --}}     
 
+
+@isset($fornecedores) <!-- executa quando for true -->
 Fornecedor: {{ $fornecedores[0]['nome'] }}
 <br>
 Status: {{ $fornecedores[0]['status'] }}
+<br>
+Status: {{ $fornecedores[0]['cnpj'] }}
+<br>
+Status: {{ $fornecedores[1]['nome'] }}
+<br>
+Status: {{ $fornecedores[1]['status'] }}
+<br>
+@isset($fornecedores[1]['cnpj'])
+    Status: {{ $fornecedores[1]['cnpj'] }}
+@endisset
+<br>
 
-@if ($fornecedores[0]['status'] == 'N')
-<br>
-    Fornecedor Inativo
-    
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S') <!-- executa quando for false -->
-Fornecedor Inativo (unless)
-@endunless
+@endisset
+
 <br>
