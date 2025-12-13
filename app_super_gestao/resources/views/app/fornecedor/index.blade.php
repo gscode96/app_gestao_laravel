2cc67d72a1c0a7de0a9aa0fc0708206f9be2d2c4
Gabriel Santos <gs.code96@gmail.com>
2025-12-12 21:39:20 -0300

    flow: utilizando valor default nas variaveis

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -14,24 +14,12 @@
 
 
 @isset($fornecedores) <!-- executa quando for true -->
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br>
-Status: {{ $fornecedores[0]['status'] }}
-<br>
-CNPJ: {{ $fornecedores[0]['cnpj'] }}
-<br>
-Status: {{ $fornecedores[1]['nome'] }}
-<br>
-Status: {{ $fornecedores[1]['status'] }}
-<br>
-@isset($fornecedores[0]['cnpj'])
-    CNPJ: {{ $fornecedores[0]['cnpj'] }}
-    @empty($fornecedores[0]['cnpj'])
-         - não informado!
-    @endempty
-    
-@endisset
-<br>
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido' }}
+    <br>
 
 @endisset
 
