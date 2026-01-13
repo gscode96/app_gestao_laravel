78bd056d63e7fcfc08ff307a326f34190bac66dc
Gabriel Santos <gs.code96@gmail.com>
2026-01-13 10:52:19 -0300

    flow: adicionando fornecedores com metodo create e fillable

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -8,4 +8,6 @@ class Fornecedor extends Model
 {
     //eloquent assume que o nome da tabela é o plural do nome do modelo
     protected $table = 'fornecedores';
+    //definindo os campos que podem ser preenchidos via atribuição em massa
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
