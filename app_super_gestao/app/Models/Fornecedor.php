a065ac6f3a71a8985eee31f05cdbe541ef0135c7
Gabriel Santos <gs.code96@gmail.com>
2026-01-13 09:00:18 -0300

    feat: ajustado nome da tabela fornecedor no ORM

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //eloquent assume que o nome da tabela é o plural do nome do modelo
+    protected $table = 'fornecedores';
 }
